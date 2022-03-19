a18a6b5b0a524937b1946023ff155d2df11da32e
edu-ministori <88914370+edu-ministori@users.noreply.github.com>
2022-03-19 17:22:49 +0900

    add

--- project/js/function.js
+++ project/js/function.js
@@ -1,7 +1,33 @@
 // haburger button access
-let hamburger = document.querySelector('.hamburger-menu');
+const hamburger = document.querySelector('.hamburger-menu');
 const gnb = document.querySelector('.gnb');
 
 hamburger.addEventListener('click', function(){
   gnb.classList.toggle('active');
 });
+
+// tab  menu
+const tabMenu = document.querySelectorAll('.tab-menu-item');
+const group = document.querySelectorAll('.section2 .group');
+let activeIndex = 0;
+
+for(let i=0;i<tabMenu.length;i++){
+  tabMenu[i].addEventListener('click', function(){
+
+    for(let j=0;j<tabMenu.length;j++){
+      tabMenu[j].classList.remove('active');
+      group[j].classList.remove('active');
+    }
+
+    this.classList.add('active');
+
+    for(let j=0;j<tabMenu.length;j++){
+      if(tabMenu[j] === this){
+        activeIndex = j;
+      }
+    }
+
+    group[activeIndex].classList.add('active');
+  });
+}
+
